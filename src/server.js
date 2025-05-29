c719066fe29697b73697f8b75bffd71c954f5c63
Iggy <ignaciomacam@gmail.com>
2025-05-29 14:29:43 +0800

    cron import fixed

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { initDB } from './config/db.js'; // Adjust the path as necessary
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionsRoute from './routes/transactionsRoute.js';
-import { job } from './config/cron.js';
+import job from './config/cron.js';
 
 dotenv.config();
 
