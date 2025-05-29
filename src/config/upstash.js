050e31c02dcb55f2bcdcfe21b6c8ccbddc558e6c
Iggy <ignaciomacam@gmail.com>
2025-05-29 17:51:02 +0800

    updated cron job

--- src/config/upstash.js
+++ src/config/upstash.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 const rateLimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(1000, '60 s'), // 100 requests per minute
+  limiter: Ratelimit.slidingWindow(500, '60 s'), // 100 requests per minute
 });
 
 export default rateLimit;
